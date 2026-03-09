d7268fe5fc985b5518c43e11977fb38003560095
LLuthfiY <LLuthfiY@outlook.com>
2026-03-09 19:47:14 +0700

    init

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# This script will install the Skeleton Shell
+
+git clone https://github.com/LLuthfiY/Skeleton-Shell $HOME/.local/share/Skeleton-Shell
+
+function asking {
+    if [ -d "$HOME/.config/$1" ]; then
+      read -p "Replace $1 folder?(y/N) " -n 1 -r
+      echo    # (optional) move to a new line
+      if [[ $REPLY =~ ^[Yy]$ ]] 
+      then
+          mv $HOME/.config/$1 $HOME/.config/$1-$(date +%Y-%m-%d-%H-%M-%S)
+          cp -r $PWD/$1 $HOME/.config
+      else
+          echo "Skipping $1"
+      fi
+    else
+      cp -r $PWD/$1 $HOME/.config
+    fi
+
+}
+
+asking "Skeleton-Shell"
+asking "gtk-3.0"
+asking "gtk-4.0"
+asking "kitty"
+
+echo "source = ~/.cache/quickshell/Skeleton-Shell/ConfigFolder/hyprland/skeleton-shell.conf" >> ~/.config/hypr/hyprland.conf
+
+qs -p $HOME/.local/share/Skeleton-Shell & disown
