101f7a87d83c24284f20a79f38fab2d645886659
LLuthfiY <LLuthfiY@outlook.com>
2026-06-04 05:59:01 +0700

    add line

--- install.sh
+++ install.sh
@@ -38,14 +38,14 @@ hyprctl reload
 
 # Check if already present before appending
 if ! grep -q "Skeleton-Shell" ~/.config/hypr/hyprland.conf; then
-    echo "source = ~/.cache/quickshell/Skeleton-Shell/ConfigFolder/hyprland/skeleton-shell.conf" >> ~/.config/hypr/hyprland.conf
+    echo -e "\nsource = ~/.cache/quickshell/Skeleton-Shell/ConfigFolder/hyprland/skeleton-shell.conf" >> ~/.config/hypr/hyprland.conf
     echo "Added Skeleton-Shell config!"
 else
     echo "Skeleton-Shell already configured"
 fi
 
 if ! grep -q "Skeleton-Shell" ~/.config/hypr/hyprland.lua; then
-  echo -e "local home = os.getenv(\"HOME\")\npackage.path = home .. \"/.cache/quickshell/Skeleton-Shell/ConfigFolder/hyprland/?.lua;\" .. package.path\nrequire(\"skeleton-shell\")" >> ~/.config/hypr/hyprland.lua
+  echo -e "\nlocal home = os.getenv(\"HOME\")\npackage.path = home .. \"/.cache/quickshell/Skeleton-Shell/ConfigFolder/hyprland/?.lua;\" .. package.path\nrequire(\"skeleton-shell\")" >> ~/.config/hypr/hyprland.lua
   echo "Added Skeleton-Shell config!"
 else
   echo "Skeleton-Shell already configured"
