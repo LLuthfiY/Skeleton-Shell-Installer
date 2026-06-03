a52c1a5fb08ecea2c868c89106d7680e3c4de66c
LLuthfiY <LLuthfiY@outlook.com>
2026-06-03 17:06:59 +0700

    fix autostart

--- Skeleton-Shell/hyprland/autostart.lua
+++ Skeleton-Shell/hyprland/autostart.lua
@@ -1,4 +1,2 @@
-hl.dsp.exec_cmd("qs -p ~/.local/share/Skeleton-Shell")
-hl.dsp.exec_cmd(
-	"pidof hypridle || hypridle -c $HOME/.cache/quickshell/Skeleton-Shell/ConfigFolder/hyprland/hypridle.conf"
-)
+hl.exec_cmd("qs -p ~/.local/share/Skeleton-Shell")
+hl.exec_cmd("pidof hypridle || hypridle -c $HOME/.cache/quickshell/Skeleton-Shell/ConfigFolder/hyprland/hypridle.conf")
